a4625c7768791a4009fa129b7f5db81397842072
Kyle Mitchell <kyle@kemitchell.com>
2015-09-07 14:13:09 -0700

    Print "No errors" on successful test

--- test.js
+++ test.js
@@ -4,3 +4,5 @@ var result = tv4.validateMultiple(require('./index'), require('./schema'))
 if (result.errors.length > 0) {
   process.stderr.write(JSON.stringify(result, null, 2) + '\n')
   process.exit(1) }
+else {
+  process.stdout.write('No errors\n') }
